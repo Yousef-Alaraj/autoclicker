ac14f31135b410b626d812a4eb3829845e550f96
Yousef Alaraj <yousefelaraj@gmail.com>
2026-09-26 01:54:41 +0300

    Setup for multi threading and cpu load managing also learned how to make edge triggering

--- main.cpp
+++ main.cpp
@@ -4,33 +4,71 @@
 #include <iostream>
 #include <thread>
 
-int main() {
+std::atomic<bool> is_clicking(false);
+std::atomic<bool> keep_running(true);
 
-    for (int i = 0; i < 1e9; i++) {
-        CGEventRef event = CGEventCreate(NULL);
+void clicker_loop() {
+    CGEventRef event = CGEventCreate(NULL);
 
-        if (!event) {
-            std::cout << "CGEventCreate Failed\n";
-            return 0;
-        }
-        CGPoint cursor = CGEventGetLocation(event);
-        CGEventRef down =
-            CGEventCreateMouseEvent(NULL, kCGEventLeftMouseDown, cursor, kCGMouseButtonLeft);
-        CGEventPost(kCGHIDEventTap, down);
-
-        CGEventRef up =
-            CGEventCreateMouseEvent(NULL, kCGEventLeftMouseUp, cursor, kCGMouseButtonLeft);
-        CGEventPost(kCGHIDEventTap, up);
-
-        CFRelease(up);
-        CFRelease(down);
-        CFRelease(event);
-
-        if (CGEventSourceKeyState(kCGEventSourceStateHIDSystemState, kVK_Escape)) {
-            return 0;
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    if (!event) {
+        std::cout << "CGEventCreate Failed\n";
+        return;
     }
+
+    CGPoint cursor = CGEventGetLocation(event);
+    CGEventRef down =
+        CGEventCreateMouseEvent(NULL, kCGEventLeftMouseDown, cursor, kCGMouseButtonLeft);
+    CGEventPost(kCGHIDEventTap, down);
+
+    CGEventRef up = CGEventCreateMouseEvent(NULL, kCGEventLeftMouseUp, cursor, kCGMouseButtonLeft);
+    CGEventPost(kCGHIDEventTap, up);
+
+    CFRelease(up);
+    CFRelease(down);
+    CFRelease(event);
+
+    if (CGEventSourceKeyState(kCGEventSourceStateHIDSystemState, kVK_Escape)) {
+        return;
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
+
+void clicker_worker() {
+    while (keep_running) {
+        if (is_clicking)
+            clicker_loop();
+        else {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+}
+
+int main() {
+    std::thread worker(clicker_worker);
+
+    bool previous_toggle_pressed = false;
+    bool previous_escape_pressed = false;
+
+    while (keep_running) {
+        bool current_toggle_pressed =
+            CGEventSourceKeyState(kCGEventSourceStateHIDSystemState, kVK_ANSI_S);
+        if (!previous_toggle_pressed && current_toggle_pressed) {
+            is_clicking = is_clicking ^ 1;
+        }
+
+        bool current_escape_pressed =
+            CGEventSourceKeyState(kCGEventSourceStateHIDSystemState, kVK_Escape);
+        if (!previous_escape_pressed && current_escape_pressed) {
+            keep_running = false;
+        }
+
+        previous_toggle_pressed = current_toggle_pressed;
+        previous_escape_pressed = current_escape_pressed;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+    worker.join();
+
     return 0;
 }
